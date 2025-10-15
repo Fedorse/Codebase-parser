73a7f84fe14c7ea96205faf1399380d4bc43ba91
Fedor Semochkin <221800ss@gmail.com>
2025-10-15 16:41:05 +0700

    added monaco editor

--- src/lib/components/edit-modal.svelte
+++ src/lib/components/edit-modal.svelte
@@ -3,6 +3,7 @@
   import { Code, Copy } from '@lucide/svelte/icons';
   import { Button } from '$lib/components/ui/button/index';
   import Badge from '$lib/components/ui/badge/badge.svelte';
+  import MonacoEditor from '$lib/components/monaco-editor/monaco-editor.svelte';
 
   let {
     fileContent,
@@ -50,17 +51,12 @@
       </div>
 
       <div class="bg-muted/50 min-h-0 flex-1 rounded-md border">
-        <textarea
-          bind:value={fileContent}
-          class="h-full w-full resize-none overflow-auto p-4 font-mono text-sm whitespace-pre-wrap"
-        >
-          {fileContent}
-        </textarea>
+        <MonacoEditor bind:value={fileContent} />
       </div>
     </div>
 
     <div class="flex justify-end pt-4">
-      <Button variant="outline" onclick={() => updateFileContent(fileContent)}>Save</Button>
+      <Button variant="default" onclick={() => updateFileContent(fileContent)}>Save</Button>
     </div>
   </Dialog.Content>
 </Dialog.Root>
---
+++ src/lib/components/monaco-editor/monaco-editor.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte';
+  import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+  import { setupThemes } from './utils';
+  import { mode } from 'mode-watcher';
+
+  let { value = $bindable(''), className = '' } = $props();
+
+  let editor: Monaco.editor.IStandaloneCodeEditor | null = $state(null);
+  let editorContainer: HTMLElement | null = $state(null);
+  let monaco: typeof Monaco | null = $state(null);
+
+  onMount(async () => {
+    monaco = (await import('./setup')).default;
+    await setupThemes(monaco);
+
+    // Editor initialization
+    editor = monaco.editor.create(editorContainer!, {
+      value,
+      minimap: { enabled: false },
+      language: 'plaintext',
+      automaticLayout: true,
+      wordWrap: 'on',
+      scrollBeyondLastLine: false,
+      fontSize: 14,
+      lineNumbers: 'on',
+      renderLineHighlight: 'all',
+      wordBasedSuggestions: 'currentDocument',
+      scrollbar: {
+        vertical: 'auto',
+        horizontal: 'auto',
+        useShadows: false,
+        verticalScrollbarSize: 10,
+        horizontalScrollbarSize: 10,
+        alwaysConsumeMouseWheel: false
+      }
+    });
+
+    editor.onDidChangeModelContent(() => {
+      value = editor!.getValue();
+    });
+  });
+
+  $effect(() => {
+    if (editor) {
+      const v = editor.getValue();
+      if (value !== v) editor.setValue(value ?? '');
+    }
+  });
+
+  $effect(() => {
+    if (!editor) return;
+    if (mode.current === 'dark') {
+      editor.updateOptions({ theme: 'dark' });
+    } else {
+      editor.updateOptions({ theme: 'light' });
+    }
+  });
+
+  onDestroy(() => {
+    const model = editor?.getModel();
+    model?.dispose();
+    editor?.dispose();
+  });
+</script>
+
+<div bind:this={editorContainer} class={`h-full w-full ${className}`} />
