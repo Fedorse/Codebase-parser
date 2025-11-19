fcf515d4f5ad987034b2cc03aa73a21d69416f36
Ivan Semochkin <ivansemochkin@Ivans-MacBook-Pro.local>
2025-11-19 12:45:51 +0700

    Remove svelte build

--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/files/[...path]/edit/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/files/[...path]/edit/+page.svelte";
